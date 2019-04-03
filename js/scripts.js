b043e7f2166e132e66dff2501d9a0492d9973fca
Anna Gibbons and Crystal Fecteau <crystal@>
2019-04-03 14:02:06 -0700

    add js for else statement deals with our inputted words beginning with vowels.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
   $("form#translate").submit(function(event) {
     event.preventDefault();
     var string = $("#userInput").val();
+    var len = string.length;
     var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-    var single = ["a","i","A","I"];
 
     for(var i = 0; i <= vowels.length; i++) {
-      for(var e = 0; e < single.length; e++)
-      if (string.length[0] === single[e]){
-        $("#result").text(string + "ay");
-      }else if (string[0] === vowels[i]) {
+       if (len === 1 ) {
+          $("#result").text(string + "ay")
+        }
+      else if (string[0] === vowels[i] && len > 1) {
         $("#result").text(string + "way");
       }
     }
+    });
   });
-});
