4fe2dd66f95509ce4bbf63103a78fd6323ee82b1
Anna Gibbons and Crystal Fecteau <crystal@>
2019-04-03 16:40:39 -0700

    add clean code used regexp

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,45 @@
 $(document).ready(function() {
-  var string;
+  var str;
   var len;
-  var vowels;
-  var cons;
-  var piglatin;
+  var regex;
 
   $("form#translate").submit(function(event) {
     event.preventDefault();
-    var string = $("#userInput").val();
-    var len = string.length;
-    var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-    var firstLetter = string.slice(0,1);
-    var lastLetter = string.slice(1);
+    var str = $("#userInput").val();
+    var len = str.length;
+    var regex = /[aeiouAEIOU]/;
+    var pigLatin = '';
 
-    for(var i = 0; i <= vowels.length; i++) {
-    if (output = lastLetter + firstLetter + "ay"){
-      $("#result").text(output);
-      }
-    }
-
-    for(var e = 0; e <= vowels.length; e++) {
+      for(var e = 0; e <= len; e++) {
        if (len === 1 ) {
-          $("#result").text(string + "ay")
-        }
-      else if (string[0] === vowels[e] && len > 1) {
-        $("#result").text(string + "way");
+        pigLatin = (str + "ay");
+        $("#result").text(pigLatin);
+
+        }else if(str[0].match(regex)) {
+        pigLatin = (str + 'way');
+        $("#result").text(pigLatin);
+
+        }else {
+        var cons = str.indexOf(str.match(regex)[0]);
+        pigLatin = (str.substr(cons)) + (str.substr(0, cons) + "ay");
+        $("#result").text(pigLatin);
       }
+
     }
+    });
+    // for(var i = 0; i <= vowels.length; i++) {
+    // if (output = lastLetter + firstLetter + "ay"){
+    //   $("#result").text(output);
+    //   }
+    // }
+    //
+    // for(var e = 0; e <= vowels.length; e++) {
+    //    if (len === 1 ) {
+    //       $("#result").text(string + "ay")
+    //     }
+    //   else if (string[0] === vowels[e] && len > 1) {
+    //     $("#result").text(string + "way");
+    //   }
+    // }
   });
-});
+// });
