ab79be364c7ba1e9d749e553ab133ca61e6bdbd3
Anna Gibbons and Crystal Fecteau <crystal@>
2019-04-03 15:40:20 -0700

    debugged slice

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,35 @@ $(document).ready(function() {
     var string = $("#userInput").val();
     var len = string.length;
     var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-    var cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z"];
+    // var cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z"];
+    var firstLetter = string.slice(0,1);
+    var lastLetter = string.slice(1);
     var piglatin = []
 
     for(var i = 0; i <= vowels.length; i++) {
+    if (output = lastLetter + firstLetter + "ay"){
+      $("#result").text(output);
+      }
+    }
+
+    for(var e = 0; e <= vowels.length; e++) {
+
        if (len === 1 ) {
           $("#result").text(string + "ay")
         }
-      else if (string[0] === vowels[i] && len > 1) {
+      else if (string[0] === vowels[e] && len > 1) {
         $("#result").text(string + "way");
       }
-      var piglatin = string.slice(0,1);
-        $("#result").text(string.concat(piglatin + "ay"));
+
+      // var piglatin = string.slice(0,1);
+      //   $("#result").text(string.concat(piglatin + "ay"));
 
 
     }
-    // for(var e = 0; e <= cons.length; e++ ){
+    // for(var e = 0; e <= len; e++ ){
     //   if (string[0] === cons[e]);
-    //   string.slice(cons);
-    //   $("#result").text(string + "ay");
+    //   var piglatin = string.splice(2,);
+    //   $("#result").text(string.concat(piglatin + "ay"));
     // }
     });
 
